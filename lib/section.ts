154431ee1c3128bdb117886f7e8f5a7f4d8fba38
kevinkeyssx <keyss1700@gmail.com>
2025-06-13 22:08:28 -0400

    Session & Buildings in sections added

---
+++ lib/section.ts
@@ -0,0 +1,31 @@
+export const SECTION_SESSION = [
+    { label: "Cátedra",     value: "C" },
+    { label: "Ayudantía",   value: "A" },
+    { label: "Eventos UAI",   value: "E" },
+    { label: "Taller",   value: "T" },
+];
+
+
+export const SECTION_BUILDING_PLANNED = [
+    { label: "A", value: "A" },
+    { label: "B", value: "B" },
+    { label: "C", value: "C" },
+    { label: "D", value: "D" },
+    { label: "E", value: "E" },
+    { label: "F", value: "F" },
+    { label: "A-B", value: "A-B" },
+    { label: "A-C", value: "A-C" },
+    { label: "A-D", value: "A-D" },
+    { label: "A-E", value: "A-E" },
+    { label: "A-F", value: "A-F" },
+    { label: "B-C", value: "B-C" },
+    { label: "B-D", value: "B-D" },
+    { label: "B-E", value: "B-E" },
+    { label: "B-F", value: "B-F" },
+    { label: "C-D", value: "C-D" },
+    { label: "C-E", value: "C-E" },
+    { label: "C-F", value: "C-F" },
+    { label: "D-E", value: "D-E" },
+    { label: "D-F", value: "D-F" },
+    { label: "E-F", value: "E-F" },
+];
