fa56bcd0803af9d31bc010dfb26792d8bc9b3759
kevinkeyssx <keyss1700@gmail.com>
2025-06-19 20:54:26 -0400

    Room renamed Spaces

--- lib/space.ts
+++ lib/space.ts
@@ -1,6 +1,6 @@
-import { Room } from "./types";
+import type { Space } from "./types";
 
-export const getSpaceTypeName = ( type: Room["type"] ) => ({
+export const getSpaceTypeName = ( type: Space["type"] ) => ({
     'ROOM'      : 'Sala',
     'AUDITORIO' : 'Auditorio',
     'DIS'       : 'Diseño',
