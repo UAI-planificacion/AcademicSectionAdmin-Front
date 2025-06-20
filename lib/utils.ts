a294dd3d2c40463e5c75b26bba1e1098e8dd99a7
kevinkeyssx <keyss1700@gmail.com>
2025-06-19 20:56:11 -0400

    formatDate & generateId added

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,3 +4,18 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
     return twMerge(clsx(inputs))
 }
+
+export function formatDate(date?: Date): string {
+    if (!date) return "N/A"
+    return new Intl.DateTimeFormat("es-ES", {
+        day: "2-digit",
+        month: "2-digit",
+        year: "numeric",
+    }).format(new Date(date))
+}
+
+export function generateId(prefix: string): string {
+    const timestamp = Date.now().toString(36)
+    const randomStr = Math.random().toString(36).substring(2, 8)
+    return `${prefix}${timestamp}${randomStr}`.toUpperCase().substring(0, 8)
+}
