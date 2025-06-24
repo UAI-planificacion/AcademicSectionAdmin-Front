9e388ee111b0cb89a569c3c6dd91fe3a074b8778
kevinkeyssx <keyss1700@gmail.com>
2025-06-23 22:27:08 -0400

    Professors Page updated

--- app/professors/page.tsx
+++ app/professors/page.tsx
@@ -90,7 +90,7 @@ export default function ProfessorsPage() {
                         {email}
                     </div>
                 ) : (
-                    "N/A"
+                    "-"
                 )
             },
         },
