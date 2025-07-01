6307f886cdfb1cefc6fc62476591c3a7320b0b98
kevinkeyssx <keyss1700@gmail.com>
2025-06-30 22:17:06 -0400

    ssr commented

--- app/sections/page.tsx
+++ app/sections/page.tsx
@@ -1,26 +1,29 @@
 'use client'
 
+import { ModuleGrid } from "@/components/sections/module-grid";
+import { SchedulerDashboard } from "@/components/sections/scheduler-dashboard";
 import dynamic from "next/dynamic"
 
-const SchedulerDashboard = dynamic(
-    () => import( '@/components/sections/scheduler-dashboard' ).then(( mod ) => mod.SchedulerDashboard ),
-    {
-        ssr: false,
-        loading: () => (
-            <div className="container mx-auto py-6 px-4">
-                <h1 className="text-3xl font-bold mb-6">Administrador de Secciones Académicas</h1>
+// const SchedulerDashboard = dynamic(
+//     () => import( '@/components/sections/scheduler-dashboard' ).then(( mod ) => mod.SchedulerDashboard ),
+//     {
+//         ssr: false,
+//         loading: () => (
+//             <div className="container mx-auto py-6 px-4">
+//                 <h1 className="text-3xl font-bold mb-6">Administrador de Secciones Académicas</h1>
 
-                <div className="h-[600px] w-full border rounded-lg flex items-center justify-center">
-                    Cargando planificador...
-                </div>
-            </div>
-        ),
-    }
-);
+//                 <div className="h-[600px] w-full border rounded-lg flex items-center justify-center">
+//                     Cargando planificador...
+//                 </div>
+//             </div>
+//         ),
+//     }
+// );
 
 export default function SectionPage() {
     return (
         <main className="max-h-[calc(100vh-73px)] overflow-y-hidden">
+            {/* <SchedulerDashboard /> */}
             <SchedulerDashboard />
         </main>
     )
