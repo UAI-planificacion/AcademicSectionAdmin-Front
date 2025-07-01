ec5bb190cf21858183b9aec867f3d413af2d2451
kevinkeyssx <keyss1700@gmail.com>
2025-06-30 22:22:04 -0400

    formatDate | string  | null | undefined added

--- lib/utils.ts
+++ lib/utils.ts
@@ -5,7 +5,7 @@ export function cn(...inputs: ClassValue[]) {
     return twMerge(clsx(inputs))
 }
 
-export function formatDate(date?: Date): string {
+export function formatDate( date?: Date | string  | null | undefined ): string {
     if (!date) return "-"
     return new Intl.DateTimeFormat("es-ES", {
         day: "2-digit",
