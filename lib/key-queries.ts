616e12eb3c584341fc0718487dec231040211907
kevinkeyssx <keyss1700@gmail.com>
2025-12-23 20:41:09 -0300

    KEY_QUERYS added

---
+++ lib/key-queries.ts
@@ -0,0 +1,30 @@
+export const KEY_QUERYS = {
+    FACULTIES       : 'faculties',
+    REQUESTS        : 'requests',
+    REQUEST_DETAIL  : 'request-detail',
+    STAFF           : 'staff',
+    SUBJECTS        : 'subjects',
+    PROFESSORS      : 'professors',
+    PERIODS         : 'periods',
+    OFFERS          : 'offers',
+    PLANNING_CHANGE : 'planning-change',
+    REQUEST_SESSION  : 'request-sessions',
+
+
+    GRADES          : 'grades',
+
+    SIZES            : 'sizes',
+    DAYS            : 'days',
+    SPACES          : 'spaces',
+    MODULES         : 'modules',
+    COMMENTS        : "comments",
+    SECTIONS       : 'sections',
+    SESSIONS        : 'sessions',
+
+
+    COST_CENTERS    : 'cost-centers',
+    NOTIFICATIONS   : 'notifications',
+    AVAILABLE_DATES : 'available-dates',
+
+}
+
