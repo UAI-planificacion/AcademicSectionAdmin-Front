88ac4e8f5414f542c87c21bc82935f2e64d84059
kevinkeyssx <keyss1700@gmail.com>
2025-12-30 22:06:02 -0300

    DayModule & ModuleSession implemented

--- models/module.model.ts
+++ models/module.model.ts
@@ -1,4 +1,4 @@
-export type Difference = 'A' | 'B' | 'C' | 'D' | null | undefined;
+export type Difference = 'A' | 'B' | 'C' | 'D' | 'E' | 'F' | 'G' | 'H' | 'I' | 'J' | 'K' | 'L' | 'M' | 'N' | 'O' | 'P' | 'Q' | 'R' | 'S' | 'T' | 'U' | 'V' | 'W' | 'X' | 'Y' | 'Z' | null | undefined;
 
 
 interface ModuleBase {
@@ -6,7 +6,7 @@ interface ModuleBase {
     code        : string;
     isActive    : boolean;
     name        : string;
-    difference? : Difference;
+    difference  : Difference;
     startHour   : string;
     endHour     : string;
 }
@@ -16,6 +16,7 @@ export interface Module extends ModuleBase {
     order       : number;
     dayModuleId : number;
 }
+
 
 export interface ModuleOriginal extends ModuleBase {
     days        : number[];
@@ -29,4 +30,26 @@ export interface ModuleCreate {
     startHour   : string;
     endHour     : string;
     dayIds      : number[];
+}
+
+
+
+export interface DayModule {
+    id          : number;
+    dayId       : number;
+    moduleId    : number;
+}
+
+
+export interface ModuleSession {
+    id          : number;
+    code        : string;
+    difference  : string | null;
+    startHour   : string;
+    endHour     : string;
+    isActive    : boolean;
+    createdAt   : string;
+    updatedAt   : string;
+    name        : string;
+    days        : number[];
 }
