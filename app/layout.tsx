71f5235571ededa2baad9ceafe7527b10249dce3
kevinkeyssx <keyss1700@gmail.com>
2026-01-30 22:59:50 -0300

    theme provider import updted

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from 'next'
 
 import './globals.css'
 
-import { ThemeProvider }    from '@/components/theme-provider'
+import { ThemeProvider }    from '@/components/theme/theme-provider'
 import Header               from '@/components/Header'
 import { Toaster }          from "@/components/ui/sonner"
 import { QueryProvider }    from '@/providers/query-provider'
